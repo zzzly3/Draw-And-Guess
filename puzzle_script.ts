3f4e06cf70003dbaa9fa45dbd324b50106eab1cc
zzzly3 <1683994033@qq.com>
2023-04-04 16:45:54 +0800

    update puzzles

--- puzzle_script.ts
+++ puzzle_script.ts
@@ -3,6 +3,6 @@ import {readFileSync} from 'fs'
 
 const data = readFileSync('plain.txt').toString().split('\n')
 for (const i of data) {
-    add_puzzle({word: i, hint: '乘黄'})
+    add_puzzle({word: i, hint: '动作'})
 }
 sync_puzzle()
